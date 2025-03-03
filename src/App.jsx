2bfbf2365b6be54f964464a0e793625321c846d9
Harshaltwc123 <harshaltwc004@gmail.com>
2025-03-03 16:30:20 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Home from "./home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
-// import Courses from "./courses/Courses";
-import Course from "./components/Course";
+import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
@@ -17,8 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route
             path="/course"
-            // element={authUser ? <Courses /> : <Navigate to="/signup" />}
-            element={authUser ? <Course /> : <Navigate to="/signup" />}
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
           />
           <Route path="/signup" element={<Signup />} />
         </Routes>
